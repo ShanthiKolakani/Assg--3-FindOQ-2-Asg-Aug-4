dabb7f2b805ef71c5b1b6217888fd8b1994e3491
Shanthi Kolakani <shanthi.kolakani@zensar.com>
2020-08-05 02:56:12 +0530

    add new file to master

---
+++ ConLinkedList/ConLinkedList/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConLinkedList
+{
+    class Program
+    {
+        static void Main() {
+            LinkedList<String> list = new LinkedList<String>();
+            list.AddLast("one");
+            list.AddLast("two");
+            list.AddLast("three");
+            list.AddLast("four");
+            list.AddLast("five");
+            list.AddLast("six");
+            list.AddLast("seven");
+            Console.WriteLine("-------elements in the linkedlist---------------");
+            foreach (var st in list)
+            {
+                Console.WriteLine(st);
+            }
+            Console.WriteLine("--------------using Enumerator-------------------");
+            LinkedList<string>.Enumerator ll = list.GetEnumerator();
+            while (ll.MoveNext())
+            {
+                Console.WriteLine(ll.Current);
+            }
+            Console.WriteLine("_________________________remove paricualr element-------------------");
+            list.Remove("one");
+            foreach (var e in list)
+            {
+                Console.WriteLine(e);
+            }
+            Console.WriteLine("_______________________remove at first-------------------------");
+            list.RemoveFirst();
+            foreach (var e in list)
+            {
+                Console.WriteLine(e);
+            }
+            Console.WriteLine("_______________________________Removes at last------------------------------");
+            list.RemoveLast();
+            foreach (var e in list)
+            {
+                Console.WriteLine(e); }
+            Console.Read();
+        }
+    }
+}
+
+
+
+
+ 
---
+++ DictionaryApp/DictionaryApp/Dictionary.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DictionaryApp
+{
+    class Dictionary
+    {
+         static void Main(string[] args)
+            {
+                Dictionary<int, string> dict = new Dictionary<int, string>();
+                dict.Add(1, "apples");
+                dict.Add(2, "strawbarry");
+                dict.Add(3, "kiwi");
+                dict.Add(4, "draganfruit");
+
+                Console.WriteLine("Printing Dictionary Element");
+                foreach (var a in dict)
+                {
+                    Console.WriteLine(a);
+                }
+                Console.WriteLine();
+
+            Console.WriteLine("......................COUNT ................................");
+
+            Console.WriteLine("Count of element in dictionary : {0}", dict.Count);
+            Console.WriteLine();
+
+            Console.WriteLine("$#$#$#$#$#$#$#$#$#$# CONTAINSKEY  $#$#$#$#$#$#$#$#$$#$#");
+
+            Console.WriteLine("Checking index 1 in dict : {0}", dict.ContainsKey(1));
+
+            Console.WriteLine(".......................... CONTAINSVALUE ........................");
+
+            Console.WriteLine("Checking Value Guntur in dict : {0}", dict.ContainsValue("Guntur"));
+            Console.WriteLine();
+
+            Console.WriteLine("$#$#$#$#$#$#$#$#$#$# CONTAINSKEY  $#$#$#$#$#$#$#$#$$#$#");
+
+            Console.WriteLine("Checking index 5 in dict : {0}", dict.ContainsKey(5));
+            Console.WriteLine("");
+                dict.Clear();
+
+
+            foreach (var a in dict)
+            {
+                Console.WriteLine(a);
+            }
+
+            Dictionary<string, string> dict2 = new Dictionary<string, string>()
+            {
+                { "apple","strawbarry" },
+            {"goa", "kiwi" },
+                { "to", "draganfruit" }
+             };
+             foreach(KeyValuePair<string, string> el in dict2)
+            {
+                Console.WriteLine("{0} and {1}", el.Key, el.Value);
+            }
+           
+            Console.Read();
+
+            }
+        }
+    }
+
+
+
+
+
+
+
+
+
+
+
